ee5eff6e53cf859def6a82682c74730d3c9a0a95
victorhugolgr <victorhugolgr@victor.hugo>
2017-11-01 16:27:11 -0300

    6.3. Implementando segurança com OAuth 2 e Password Flow

--- algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -1,31 +1,39 @@
 package com.example.algamoney.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		auth.inMemoryAuthentication()
 			.withUser("admin").password("admin").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		//Todas as requisições são autenticadas
 		http.authorizeRequests()
 				.antMatchers("/categorias").permitAll()//todas as requisições para /categorias são autenticadas
 				.anyRequest().authenticated()
 				.and()
-			.httpBasic().and()//forma de autenticação básica
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.csrf().disable();
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 		
 }
